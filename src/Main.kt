48a4aa5b2657c6c496db2733d2e9706846a4ebdc
G1LB3T0 <127223507+G1LB3T0@users.noreply.github.com>
2024-07-18 11:21:46 -0600

    Primer Programa

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //
+    val nombre:String = "Oli"
+    val entero:Int = 5
+
+    val o = nombre.length
+    println(o)
+
+    if (1 > entero){
+        println("$nombre es la prueba")
+    }else{
+        println("Work")
+    }
+}
